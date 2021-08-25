9fd3086085307881511ebb3509f5a4ac8357700a
regeybot <89459508+regeybot@users.noreply.github.com>
2021-08-26 02:08:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
-TOKEN = os.environ.get("BOT_TOKEN")
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
-REDIS_URL = os.environ.get("REDIS_URL")
+TOKEN = os.environ.get("1979807081:AAEjNFqkdq2UAWoLqQc1Yzoc9TlR6saMhfk")
+WEBHOOK_URL = os.environ.get("https://noteay.herokuapp.com")
+REDIS_URL = os.environ.get("redis://h:asdfqwer1234asdf@ec2-111-1-1-1.compute-1.amazonaws.com:111")
 WORKER_URL = os.environ.get("WORKER_URL")
 WORKER_HEADERS = {"X-API-Key": os.environ.get("WORKER_API_KEY")}
